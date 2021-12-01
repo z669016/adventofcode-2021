4ada872030e0dd19c874ae7aa2644a389741941d
Rene van Putten <z669016@gmail.com>
2021-12-01 05:56:15 +0100

    Start of AoC 2021

---
+++ src/main/java/com/putoet/day1/Day1.java
@@ -0,0 +1,18 @@
+package com.putoet.day1;
+
+import com.putoet.day.Day;
+
+public class Day1 extends Day {
+    public static void main(String[] args) {
+        final Day day = new Day1();
+        day.challenge(args);
+    }
+
+    public void part1() {
+
+    }
+
+    public void part2() {
+
+    }
+}
