a5f34d24de698be6d698e91bad8db41418fb4393
Rene van Putten <z669016@gmail.com>
2021-12-01 06:32:43 +0100

    Solved day 1 part 1 and 2

--- src/main/java/com/putoet/day1/Day1.java
+++ src/main/java/com/putoet/day1/Day1.java
@@ -1,18 +1,53 @@
 package com.putoet.day1;
 
 import com.putoet.day.Day;
+import com.putoet.resources.ResourceLines;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Day1 extends Day {
+    private final List<Integer> measurements;
+
+    public Day1() {
+        this.measurements = ResourceLines.intList("/day1.txt");
+    }
+
     public static void main(String[] args) {
         final Day day = new Day1();
         day.challenge(args);
     }
 
-    public void part1() {
+    private static int countIncreases(List<Integer> measurements) {
+        int count = 0;
+        int prev = measurements.get(0);
+        for (int x = 1; x < measurements.size(); x++) {
+            final int next = measurements.get(x);
+            if (next - prev > 0)
+                count++;
 
+            prev = next;
+        }
+
+        return count;
+    }
+
+    public void part1() {
+        System.out.println("Number of increases is " + countIncreases(measurements));
     }
 
     public void part2() {
+        final List<Integer> newMeasurements = new ArrayList<>();
 
+        int one = measurements.get(0);
+        int two = measurements.get(1);
+        for (int x = 2; x < measurements.size(); x++) {
+            final int three = measurements.get(x);
+            newMeasurements.add(one + two + three);
+            one = two;
+            two = three;
+        }
+
+        System.out.println("Number of increases is " + countIncreases(newMeasurements));
     }
 }
