4171e9d100de2d9309d83c5668416d4980e8acba
Rene van Putten <z669016@gmail.com>
2021-12-10 06:56:52 +0100

    Removed use of Grid and used char[][] instead

--- src/main/java/com/putoet/day9/HeightMap.java
+++ src/main/java/com/putoet/day9/HeightMap.java
@@ -1,11 +1,10 @@
 package com.putoet.day9;
 
-import com.putoet.grid.Grid;
 import com.putoet.grid.Point;
 
 import java.util.*;
 
-public record HeightMap(Grid grid) {
+public record HeightMap(char[][] grid) {
 
     public static HeightMap of(List<String> map) {
         assert map != null;
@@ -15,15 +14,15 @@ public record HeightMap(Grid grid) {
         for (int i = 0; i < map.size(); i++) {
             grid[i] = map.get(i).toCharArray();
         }
-        return new HeightMap(new Grid(grid));
+        return new HeightMap(grid);
     }
 
     public List<Point> lowest() {
         final List<Point> lowest = new ArrayList<>();
 
-        for (int y = 0; y < grid.maxY(); y++) {
-            for (int x = 0; x < grid.maxX(); x++) {
-                char valueAt = grid.get(x, y);
+        for (int y = 0; y < grid.length; y++) {
+            for (int x = 0; x < grid[y].length; x++) {
+                char valueAt = grid[y][x];
                 if (valueAt < valueAtLowestAdjacent(x, y))
                     lowest.add(Point.of(x, y));
             }
@@ -34,13 +33,13 @@ public record HeightMap(Grid grid) {
     private char valueAtLowestAdjacent(int x, int y) {
         int lowest = Integer.MAX_VALUE;
         if (x > 0)
-            lowest = Math.min(lowest, grid.get(x - 1, y));
-        if (x + 1 < grid.maxX())
-            lowest = Math.min(lowest, grid.get(x + 1, y));
+            lowest = Math.min(lowest, grid[y][x - 1]);
+        if (x + 1 < grid[y].length)
+            lowest = Math.min(lowest, grid[y][x + 1]);
         if (y > 0)
-            lowest = Math.min(lowest, grid.get(x, y - 1));
-        if (y + 1 < grid.maxY())
-            lowest = Math.min(lowest, grid.get(x, y + 1));
+            lowest = Math.min(lowest, grid[y - 1][x]);
+        if (y + 1 < grid.length)
+            lowest = Math.min(lowest, grid[y + 1][x]);
 
         return (char) lowest;
     }
@@ -50,19 +49,29 @@ public record HeightMap(Grid grid) {
     }
 
     public int riskLevel(Point point) {
-        assert grid.contains(point.x(), point.y());
+        contains(point);
 
-        return grid.get(point.x(), point.y()) - '0' + 1;
+        return grid[point.y()][point.x()] - '0' + 1;
+    }
+
+    private void contains(Point point) {
+        assert point != null;
+        assert point.y() >= 0;
+        assert point.x() >= 0;
+        assert point.y() < grid.length;
+        assert point.x() < grid[point.y()].length;
     }
 
     public Set<Point> basin(Point lowest) {
+        contains(lowest);
+
         final Set<Point> basin = new HashSet<>();
         final Queue<Point> queue = new LinkedList<>();
 
         queue.offer(lowest);
         while (!queue.isEmpty()) {
             final Point point = queue.poll();
-            if (grid.get(point.x(), point.y()) < '9') {
+            if (grid[point.y()][point.x()] < '9') {
                 basin.add(point);
 
                 if (point.x() > 0) {
@@ -70,7 +79,7 @@ public record HeightMap(Grid grid) {
                     if (!basin.contains(next))
                         queue.offer(next);
                 }
-                if (point.x() + 1 < grid.maxX()) {
+                if (point.x() + 1 < grid[point.y()].length) {
                     final Point next = Point.of(point.x() + 1, point.y());
                     if (!basin.contains(next))
                         queue.offer(next);
@@ -80,7 +89,7 @@ public record HeightMap(Grid grid) {
                     if (!basin.contains(next))
                         queue.offer(next);
                 }
-                if (point.y() + 1 < grid.maxY()) {
+                if (point.y() + 1 < grid.length) {
                     final Point next = Point.of(point.x(), point.y() + 1);
                     if (!basin.contains(next))
                         queue.offer(next);
